f221b89ee76153c71c1717d998113ec6db4e60a0
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-30 11:45:58 +0100

    first: step-by-step argument, clear screen, display registers on top left

--- first.c
+++ first.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "fake6502.h"
 
+#define cursorXY(x,y) printf("\033[%d;%dH",(x),(y))
+
 uint8_t ram[0xFFFF];
 
 void init_ram() {
@@ -24,18 +26,30 @@ uint8_t read6502(uint16_t address) {
     return ram[address];
 }
 
+int step = 0;
+
+void clear_screen() {
+    printf("\033[2J");
+}
 void print_registers() {
+    cursorXY(1,1);
     printf("PC:   AC  XR  YR  SR  SP   NV-BDIZC\n");
     printf("%04X  %02X  %02X  %02X  %02X  %02X   ", pc, a, x, y, status, sp);
     for(int mask = 128; mask > 0; mask >>= 1) {
         printf("%c", ((status & mask) == mask) ? '1' : '0');
     }
     printf("\n\n");
+    if (step)
+        getchar();
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc > 1 && argv[1][0] == 'S') {
+        step = 1;
+    }
     hookexternal(*print_registers);
     init_ram();
+    clear_screen();
     printf("reset…\n");
     reset6502();
     exec6502(5);
